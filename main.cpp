a7314a87abddd58fbf71bc7f6ef9cca74cb64eef
Clockd <31565998+Clockd@users.noreply.github.com>
2017-09-02 20:19:15 +0530

    main.cpp
    Efficiency confirmed

--- main.cpp
+++ main.cpp
@@ -7,27 +7,16 @@ using namespace std;
 
 int main()
 {
-    int a[5];
-    char b[5];
-    char c[5];
     srand(time(0));
     int no = 0;
-    string g = "", u;
+    string g , u;
+    g.clear();
 
     for (int x = 0; x < 6; x++) {
-        a[x] = rand();
         //cout << char((a[x] % 42) +  48) << endl;
-        b[x] = char((a[x] % 26) +  65);
+        g[x] += char(rand() % 26) +  65);
     }
-
-
-    for (int y = 0; y < 6; y++) {
-        g += b[y];
-    }
-
-    cout << g << endl;
-
-
+    
     cin >> u;
     if (g == u) {
         cout << "Correct guess!";
@@ -35,14 +24,13 @@ int main()
     }
 
     else {
-        strcpy(c, u.c_str());
         for (int z = 0; z < 6; z++) {
-            if (c[z] == b[z]) {
+            if (u[z] == g[z]) {
                     no++;
-                    cout << c[z] << " is correct\n";
+                    cout << u[z] << " is correct\n";
             }
-            else if (c[z] != b[z]){
-                cout << c[z] << " is not correct\n";
+            else if (u[z] != g[z]){
+                cout << u[z] << " is not correct\n";
             }
         }
     }
